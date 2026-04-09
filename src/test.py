8a83ed49aec2628128a543f49df5fc36de17479b
Илья Иванов <blocnotic@gmail.com>
2026-04-09 22:41:51 +0300

    bibl

--- src/test.py
+++ src/test.py
@@ -1,2 +1,14 @@
-ya lublu YuRu Sanochkina
+import re
+import time
+import random
+import shutil
+import pandas as pd
 
+from tqdm.auto import tqdm
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, WebDriverException
