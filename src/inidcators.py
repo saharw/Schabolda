15a8b5af484188b75d2fc525776b46871a79b86c
Evgeny Mochalov <movhalove@yandex.ru>
2026-04-10 19:51:43 +0300

    added get_rsi func

--- src/inidcators.py
+++ src/inidcators.py
@@ -4,3 +4,26 @@ import time
 
 API_KEY = "71D8X13H5HB9KM10"
 BASE_URL = "https://www.alphavantage.co/query"
+
+
+def get_rsi(symbol):
+    params = {
+        "function": "RSI",
+        "symbol": symbol,
+        "interval": "daily",
+        "time_period": 14,
+        "series_type": "close",
+        "apikey": API_KEY
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+
+    df = pd.DataFrame(data["Technical Analysis: RSI"]).T
+    df.index = pd.to_datetime(df.index)
+    df = df.sort_index()
+
+    df.columns = ["rsi"]
+    df = df.astype(float)
+
+    return df
